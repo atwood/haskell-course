e8ea32cab17fee80b488975dfed9d09371015854
John Atwood <jw_atwood@yahoo.com>
2024-11-13 16:54:19 +0000

    edit second

--- Homework/Homework01/Homework01.hs
+++ Homework/Homework01/Homework01.hs
@@ -5,12 +5,16 @@ comment
 -}
 -- Question 2
 -- Define a function that takes a value and multiplies it by 3.
+f v = v * 3
 
 -- Question 3
 -- Define a function that calculates the area of a circle.
+areaCircle r = 3.14 * (r * r)
 
 -- Question 4
 -- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder.
+volCyl r h = (areaCircle r) * h
 
 -- Question 5
 -- Define a function that takes the height and radius of a cylinder and checks if the volume is greater than or equal to 42.
+cyl42 h r = (volCyl r h) >= 42
